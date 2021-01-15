152cca14c3ccaabda734a4133d65dea6a73429b0
YK <ko.with.ja@gmail.com>
2021-01-15 15:34:47 +0900

    update

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,8 +1,8 @@
 let scheduler = {};
 
-scheduler.CALENDARS_POLL_INTERVAL_MS_ = 6 * 60 * 60 * 1000;
-scheduler.EVENTS_POLL_INTERVAL_MS_ = 60 * 60 * 1000;
-scheduler.BADGE_UPDATE_INTERVAL_MS_ = 60 * 1000;
+scheduler.CALENDARS_INTERVAL = 6 * 60 * 60 * 1000;
+scheduler.EVENTS_INTERVAL = 60 * 60 * 1000;
+scheduler.REFRESH_INTERVAL = 60 * 1000;
 
 scheduler.start = () => {
   chrome.extension.getBackgroundPage().background.log('scheduler.start()');
@@ -17,11 +17,11 @@ scheduler.start = () => {
       feeds.fetchCalendars();
     } else {
       let feedsFetchedAtMs = feeds.lastFetchedAt.getTime();
-      if (now - feedsFetchedAtMs > scheduler.CALENDARS_POLL_INTERVAL_MS_) {
+      if (now - feedsFetchedAtMs > scheduler.CALENDARS_INTERVAL) {
         feeds.fetchCalendars();
-      } else if (now - feedsFetchedAtMs > scheduler.EVENTS_POLL_INTERVAL_MS_) {
+      } else if (now - feedsFetchedAtMs > scheduler.EVENTS_INTERVAL) {
         feeds.fetchEvents();
       }
     }
-  }, scheduler.BADGE_UPDATE_INTERVAL_MS_);
+  }, scheduler.REFRESH_INTERVAL);
 };
